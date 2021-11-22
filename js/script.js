78e2a36561aeb8950c33d03ccd6d98152970ee41
Luca Sunseri <89150843+LucaSunseri@users.noreply.github.com>
2021-11-22 15:19:08 +0100

    End exercise

--- js/script.js
+++ js/script.js
@@ -25,6 +25,7 @@ const app = new Vue({
       this.loading = true;
 
       for (let i = 0; i < 10; i++) {
+
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
         .then(response => {
              // console.log('response', response);
